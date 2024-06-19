bd6c741149cf7ee647510a5b5d46ba1694725cef
Abdul-Ahadexpo <adahadd4845e@gmail.com>
2024-06-19 23:54:58 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-document.getElementById('uploadButton').addEventListener('click', function() {
-  document.getElementById('videoUpload').click();
+document.getElementById("uploadButton").addEventListener("click", function () {
+  document.getElementById("videoUpload").click();
 });
 
-document.getElementById('videoUpload').addEventListener('change', function(event) {
-  const file = event.target.files[0];
-  if (file) {
-    const videoPlayer = document.getElementById('videoPlayer');
-    const fileURL = URL.createObjectURL(file);
-    videoPlayer.src = fileURL;
-    videoPlayer.load();
-    videoPlayer.play();
-  }
-});
+document
+  .getElementById("videoUpload")
+  .addEventListener("change", function (event) {
+    const file = event.target.files[0];
+    if (file) {
+      const videoPlayer = document.getElementById("videoPlayer");
+      const fileURL = URL.createObjectURL(file);
+      videoPlayer.src = fileURL;
+      videoPlayer.load();
+      videoPlayer.play();
+    }
+  });
